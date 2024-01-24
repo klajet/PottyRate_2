7e0486d26b5933bfc86d552f7e9c16d9bf80212c
Klajet <klajet14@gmail.com>
2024-01-24 03:18:19 +0100

    db migrations

---
+++ database/migrations/2024_01_24_021025_posts.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable(true)->references('id')->on('users')->nullOnDelete();
+            $table->foreignId('place_id')->references('id')->on('places')->cascadeOnDelete();
+            $table->foreignId('rating_id')->references('id')->on('ratings')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
